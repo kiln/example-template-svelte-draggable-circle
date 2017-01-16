8ad78e96175c65904110f5e228bb3a0d445bf610
Robin Houston <robin.houston@gmail.com>
2017-01-16 19:33:25 +0000

    Change circle into draggable circle

--- src/index.js
+++ src/index.js
@@ -1,49 +1,73 @@
-// This template uses d3-selection and d3-transition
-// Importing d3-transition adds the .transition() method to selections
-import { select } from "d3-selection";
-import "d3-transition";
+import * as d3 from "d3";
+import smoothResize from "@flourish/smooth-resize";
 
-// Anything the end user can configure in the settings panel must
-// be in this object. The separate settings.js file references
-// these property names.
 export var state = {
-	// Note: property names starting with an underscore are reserved for use by Flourish
-	radius: 10,
+	radius: 30,
 	stroke: 1,
-	color: "#FF0000"
+	color: "#FF0000",
+
+	// Because Flourish templates are responsive and can change size,
+	// we represent the position of the centre of the circle as proportions
+	// of the width and height of the whole template.
+	x_proportion: 1/2,
+	y_proportion: 1/2
 };
 
-var circle;
+var svg, circle;
+var w = window.innerWidth,
+    h = window.innerHeight;
 
-// Initialise the graphic
-export function draw() {
-	// Append and style elements based on the current state
-	var w = window.innerWidth,
-	    h = window.innerHeight;
-	var svg = select(document.body).append("svg").attr("width", w).attr("height", h);
-	circle = svg.append("circle")
-		.attr("cx", w/2)
-		.attr("cy", h/2)
+// Clamp the x and y coordinates so the whole circle is visible, including the stroke
+function clampX(x) {
+	var visible_radius = state.radius + state.stroke / 2;
+	return Math.max(visible_radius, Math.min(w - visible_radius, Math.round(x)));
+}
+function clampY(y) {
+	var visible_radius = state.radius + state.stroke / 2;
+	return Math.max(visible_radius, Math.min(h - visible_radius, Math.round(y)));
+}
+
+// This is used by both the draw() and update() functions, taking advantage of the fact
+// that D3 selections and transitions both have an .attr() method.
+function setAttributesFromState(selection_or_transition) {
+	var x = clampX(w * state.x_proportion),
+	    y = clampY(h * state.y_proportion);
+
+	selection_or_transition
+		.attr("transform", "translate(" + x + "," + y + ")")
 		.attr("r", state.radius)
 		.attr("fill", state.color)
 		.attr("stroke", "black")
 		.attr("stroke-width", state.stroke);
 }
 
-// For non-fluid visualisations, e.g. where an SVG is drawn to fill the available space,
-// it may be useful to redraw the visualisation when the window size changes.
-window.addEventListener("resize", function() {
-	if (!circle) return; // Do nothing if draw() hasn’t been called yet
-	select("svg").remove();
-	draw();
+export function draw() {
+	svg = d3.select(document.body).append("svg").attr("width", w).attr("height", h);
+
+	// Create the SVG circle, and set up the drag handler
+	circle = svg.append("circle")
+		.call(d3.drag().on("drag", function() {
+			state.x_proportion = clampX(d3.event.x) / w;
+			state.y_proportion = clampY(d3.event.y) / h;
+
+			setAttributesFromState(circle);
+		}));
+
+	// Initialise the circle’s attributes from the state
+	setAttributesFromState(circle);
+}
+
+// Keep the centre of the circle in the same relative position as the window is resized
+smoothResize(function(width, height) {
+	// Update the 'w' and 'h' variables, and resize the SVG
+	svg.attr("width", w = width).attr("height", h = height);
+
+	// Check 'circle' is defined to deal with the unlikely case
+	// the window is resized before draw() has been called
+	if (circle) setAttributesFromState(circle);
 });
 
-// The update function is called when the user changes a state property in
-// the settings panel or presentation editor. It updates elements to reflect
-// the current state.
+// Animate to the new state
 export function update() {
-	circle.transition()
-		.attr("r", state.radius)
-		.attr("fill", state.color)
-		.attr("stroke-width", state.stroke);
+	setAttributesFromState(circle.transition());
 }
